330daec9be3bc94273f2dd342de60e58b518ddc7
higherzhouhui <782492184@qq.com>
2023-06-25 12:44:49 +0800

    隐藏菜单

--- config/config.ts
+++ config/config.ts
@@ -72,12 +72,12 @@ export default defineConfig({
           component: './account/list',
           access: 'adminRouteFilter',
         },
-        {
-          name: 'account-treeList',
-          path: '/account/treeList',
-          component: './account/treelist',
-          access: 'adminRouteFilter',
-        },
+        // {
+        //   name: 'account-treeList',
+        //   path: '/account/treeList',
+        //   component: './account/treelist',
+        //   access: 'adminRouteFilter',
+        // },
         {
           name: 'account-children',
           path: '/account/children',
